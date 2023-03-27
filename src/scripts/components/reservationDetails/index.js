df8842f19766151a9648625a20f2113523db6dd1
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-27 12:08:15 +0900

    feat: create reservationDetails

--- src/scripts/components/reservationDetails/index.js
+++ src/scripts/components/reservationDetails/index.js
@@ -1,25 +1,18 @@
-import getReservationList from "../../api";
-
-async function Details() {
-  const response = await getReservationList();
+async function Details(response) {
   const test = document.querySelectorAll(".grid-area");
 
   const reservationInfo = test[0].querySelectorAll("dd");
   const clientInfo = test[1].querySelectorAll("dd");
   const requestedTerm = test[2].querySelectorAll("dd");
 
-  console.log("test :", test);
-  console.log("response :", response);
-  console.log("clientInfo :", clientInfo);
-  console.log("reservationInfo :", reservationInfo);
-  console.log("requestedTerm :", requestedTerm);
+  console.log(response, clientInfo, requestedTerm);
 
   reservationInfo.forEach((el, i) => {
     const element = el;
     if (i === 0) element.innerHTML = "상태";
-    if (i === 1) element.innerHTML = "시간";
-    if (i === 2) element.innerHTML = "접수 시간";
+    else if (i === 1) element.innerHTML = "시간";
+    else if (i === 2) element.innerHTML = "접수 시간";
   });
 }
 
-export default Details();
+export default Details;
