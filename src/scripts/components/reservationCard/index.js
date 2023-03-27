6b6f3b99fd344fd8f3bcc447333f00a126c5855f
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-27 12:08:47 +0900

    feat: create button in reservationCard

--- src/scripts/components/reservationCard/index.js
+++ src/scripts/components/reservationCard/index.js
@@ -1,17 +1,44 @@
-import getReservationList from "../../api/index";
+import printOutReservationStatus from "../../utils/printOutReservationStatus";
 
-async function Card() {
-  const response = await getReservationList();
-  const cardWrapper = document.querySelector(".cards-wrapper");
-  response.forEach((element) => {
-    console.log("element :", element);
-    // cardWrapper.createElement("li");
-  });
-  const time = document.querySelector(".time");
-  const state = document.querySelector(".state");
-  console.log("time :", time);
-  time.innerHTML = response[0].timeReserved;
-  state.innerHTML = response[0].status;
+async function Card(response) {
+  const cardsWrapper = document.querySelector(".cards-wrapper");
+
+  response
+    .filter((res) => res.status !== "done")
+    .forEach((res) => {
+      const newList = document.createElement("li");
+      const wrapperDiv = document.createElement("div");
+      const time = document.createElement("p");
+      const state = document.createElement("p");
+      const button = document.createElement("button");
+
+      newList.className = "container";
+      time.className = "time";
+      state.className = "state";
+      time.innerHTML = res.timeReserved;
+      printOutReservationStatus(state, res.status);
+      printOutReservationStatus(button, res.status);
+
+      wrapperDiv.appendChild(time);
+      wrapperDiv.appendChild(state);
+      newList.appendChild(wrapperDiv);
+      newList.appendChild(button);
+
+      cardsWrapper.appendChild(newList);
+      button.addEventListener("click", () => {
+        if (res.status === "reserved") {
+          res.status = "seated";
+          button.innerHTML = "착석 중";
+          button.className = "seated";
+        } else if (res.status === "seated") {
+          res.status = "done";
+          button.innerHTML = "퇴석";
+        } else if (res.status === "done") {
+          newList.remove();
+        }
+        printOutReservationStatus(state, res.status);
+      });
+    });
 }
 
-export default Card();
+export default Card;
