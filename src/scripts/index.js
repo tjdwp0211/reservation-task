ca65ec758a7d0d76d5e8c220af5d428d2dc44dc0
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-27 12:11:27 +0900

    refactor: Call API once on top index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,15 @@ import "../styles/index.css";
 import "../styles/components/reservationDetails/index.css";
 import "../styles/components/reservationCard/index.css";
 
+import getReservationList from "./api";
+
 import Card from "./components/reservationCard/index";
 import Details from "./components/reservationDetails/index";
 
-export { Card, Details };
+export default (async function componentsBasket() {
+  const res = await getReservationList();
+  return {
+    card: Card(res),
+    details: Details(res),
+  };
+})();
