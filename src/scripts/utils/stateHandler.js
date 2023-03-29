11c5f93b2f22e0225b8004204f94aeebc4b1e2ad
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-29 15:27:33 +0900

    feat: Creaete closure
    - Create closure in utils

---
+++ src/scripts/utils/stateHandler.js
@@ -0,0 +1,19 @@
+import Details from "../components/reservationDetails";
+
+let initialState = null;
+
+const stateHandler = (state) => {
+  if (initialState === null) initialState = state;
+
+  const handler = (newState) => {
+    initialState = newState;
+    Details();
+  };
+
+  return {
+    state: initialState,
+    handler: handler,
+  };
+};
+
+export default stateHandler;
