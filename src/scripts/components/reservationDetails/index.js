1043aa575df2cdb981a9e1c41bbb46d1392995fe
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-29 15:53:45 +0900

    feat: Apply state
    - Details can draw dynamically values

--- src/scripts/components/reservationDetails/index.js
+++ src/scripts/components/reservationDetails/index.js
@@ -1,17 +1,38 @@
-async function Details(response) {
-  const test = document.querySelectorAll(".grid-area");
+import "../../../styles/components/reservationDetails/index.css";
+import printOutReservationStatus from "../../utils/printReservationStatus";
+import fitTimeTemplate from "../../utils/fitTimeTemplate";
+import stateHandler from "../../utils/stateHandler";
 
-  const reservationInfo = test[0].querySelectorAll("dd");
-  const clientInfo = test[1].querySelectorAll("dd");
-  const requestedTerm = test[2].querySelectorAll("dd");
+function Details() {
+  const { state } = stateHandler();
+  const { status, timeReserved, timeRegistered, customer } = state;
 
-  console.log(response, clientInfo, requestedTerm);
+  const gridWrappers = document.querySelectorAll(".grid-area");
 
-  reservationInfo.forEach((el, i) => {
-    const element = el;
-    if (i === 0) element.innerHTML = "상태";
-    else if (i === 1) element.innerHTML = "시간";
-    else if (i === 2) element.innerHTML = "접수 시간";
+  if (window.innerWidth <= 720) {
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+    const container = document.querySelector(".details-container");
+    const closeButton = document.querySelector(".close");
+    closeButton.addEventListener("click", () => {
+      closeButton.style.visibility = "hidden";
+      container.style.visibility = "hidden";
+    });
+    document.querySelector("#app").appendChild(overlay);
+  }
+
+  const reservationInfo = gridWrappers[0].querySelectorAll("dd");
+  const clientInfo = gridWrappers[1].querySelectorAll("dd");
+  const requestedTerm = gridWrappers[2].querySelectorAll("dd");
+
+  [...reservationInfo, ...clientInfo, ...requestedTerm].forEach((el, i) => {
+    if (i === 0) printOutReservationStatus(el, status);
+    else if (i === 1) fitTimeTemplate(el, timeReserved);
+    else if (i === 2) fitTimeTemplate(el, timeRegistered);
+    else if (i === 3) el.innerHTML = customer.name;
+    else if (i === 4) el.innerHTML = customer.level || "-";
+    else if (i === 5) el.innerHTML = customer.memo || "-";
+    else if (i === 6) el.innerHTML = customer.request || "-";
   });
 }
 
