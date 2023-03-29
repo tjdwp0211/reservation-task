e921ed406b27cff22af890c698a073bf4c47babc
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-29 15:33:10 +0900

    refactor: import CSS
    - Moved in CSS import code in component module

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,4 @@
 import "../styles/index.css";
-import "../styles/components/reservationDetails/index.css";
-import "../styles/components/reservationCard/index.css";
 
 import getReservationList from "./api";
 
@@ -9,6 +7,7 @@ import Details from "./components/reservationDetails/index";
 
 export default (async function componentsBasket() {
   const res = await getReservationList();
+
   return {
     card: Card(res),
     details: Details(res),
