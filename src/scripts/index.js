424ccdb1e2e010c46ac2ac81abcdee494fc2c336
Sung Je Kim <rlatjdwp0211@naver.com>
2023-03-30 14:18:12 +0900

    feat: responsive UI
    - If width bigger than 720px, closeButton and overlay is hidden

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,21 @@ import getReservationList from "./api";
 import Card from "./components/reservationCard/index";
 import Details from "./components/reservationDetails/index";
 
+const mediaQuery = window.matchMedia("(max-width: 720px)");
+
+function handleMediaChange(e) {
+  const popupCloseButton = document.querySelector(".close");
+  const overlay = document.querySelector(".overlay");
+
+  if (e.matches) {
+    popupCloseButton.style.visibility = "visible";
+  } else {
+    popupCloseButton.style.visibility = "hidden";
+    overlay !== null && overlay.remove();
+  }
+}
+mediaQuery.addEventListener("change", handleMediaChange);
+
 export default (async function componentsBasket() {
   const res = await getReservationList();
   const responsefiltering = res.filter((item) => item.status !== "done");
